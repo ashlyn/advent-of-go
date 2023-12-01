5b678c3af0e0ad85adfc3df7272d0b865eccd5fd
Ashlyn Slawnyk <ashlyn.slawnyk@gmail.com>
2023-12-01 15:39:09 +0000

    2023 Day 1

--- calendar/2023/day-01/day01.go
+++ calendar/2023/day-01/day01.go
@@ -2,6 +2,8 @@ package main
 
 import (
 	"advent-of-go/utils/files"
+	"regexp"
+	"strconv"
 )
 
 func main() {
@@ -13,7 +15,10 @@ func main() {
 func solvePart1(input []string) int {
 	result := 0
 
-
+	pattern := regexp.MustCompile(`[0-9]`)
+	for _, line := range input {
+		result += getCalibrationValue(line, pattern)
+	}
 
 	return result
 }
@@ -21,7 +26,46 @@ func solvePart1(input []string) int {
 func solvePart2(input []string) int {
 	result := 0
 
-
+	pattern := regexp.MustCompile(`([0-9]|one|two|three|four|five|six|seven|eight|nine)`)
+	for _, line := range input {
+		result += getCalibrationValue(line, pattern)
+	}
 
 	return result
+}
+
+func getCalibrationValue(input string, pattern *regexp.Regexp) int {
+	lastMatch := pattern.FindString(input)
+	calStr := wordToDigit(lastMatch)
+	for i := 1; i < len(input); i++ {
+		match := pattern.FindString(input[i:])
+		if match == "" {
+			break
+		} else {
+			lastMatch = match
+		}
+	}
+	calStr += wordToDigit(lastMatch)
+	calVal, _ := strconv.Atoi(calStr)
+	return calVal
+}
+
+func wordToDigit(word string) string {
+	digits := map[string]string{
+		"one": "1",
+		"two": "2",
+		"three": "3",
+		"four": "4",
+		"five": "5",
+		"six": "6",
+		"seven": "7",
+		"eight": "8",
+		"nine": "9",
+	}
+	
+	digit, ok := digits[word]
+	if !ok {
+		return word
+	}
+	return digit
 }
