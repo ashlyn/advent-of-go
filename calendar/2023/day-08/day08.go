be0b97e309d29f87454483aab7843fd1fca12f80
Ashlyn Slawnyk <ashlyn.slawnyk@gmail.com>
2023-12-08 15:08:16 +0000

    2023 Day 8 Part 1

--- calendar/2023/day-08/day08.go
+++ calendar/2023/day-08/day08.go
@@ -2,7 +2,14 @@ package main
 
 import (
 	"advent-of-go/utils/files"
+	"regexp"
+	"strings"
 )
+
+type node struct {
+	right string
+	left string
+}
 
 func main() {
 	input := files.ReadFile(8, 2023, "\n")
@@ -11,11 +18,8 @@ func main() {
 }
 
 func solvePart1(input []string) int {
-	result := 0
-
-
-
-	return result
+	instructions, nodes, _ := parseMap(input)
+	return solve(instructions, nodes)
 }
 
 func solvePart2(input []string) int {
@@ -24,4 +28,38 @@ func solvePart2(input []string) int {
 
 
 	return result
+}
+
+func parseMap(input []string) (string, map[string]node, []string) {
+	nodes := map[string]node{}
+	endsWithA := []string{}
+	for _, line := range input[2:] {
+		name, node := parseLine(line)
+		if strings.HasSuffix(name, "A") {
+			endsWithA = append(endsWithA, name)
+		}
+		nodes[name] = node
+	}
+	return input[0], nodes, endsWithA
+}
+
+func parseLine(line string) (string, node) {
+	nodePattern := regexp.MustCompile(`[A-Z]+`)
+	parts := nodePattern.FindAllString(line, -1)
+	return parts[0], node{ left: parts[1], right: parts[2]}
+}
+
+func solve(instructions string, nodes map[string]node) int {
+	current, steps := "AAA", 0
+	for current != "ZZZ" {
+		i := steps % len(instructions)
+		if instructions[i] == 'L' {
+			current = nodes[current].left
+		} else if instructions[i] == 'R' {
+			current = nodes[current].right
+		}
+		steps++
+	}
+
+	return steps
 }
