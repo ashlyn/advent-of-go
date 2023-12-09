cc905c92eda498a50742ed66e577c6e2692aad2e
Ashlyn Slawnyk <ashlyn.slawnyk@gmail.com>
2023-12-09 22:13:52 +0000

    2023 Day 9

--- calendar/2023/day-09/day09.go
+++ calendar/2023/day-09/day09.go
@@ -2,6 +2,9 @@ package main
 
 import (
 	"advent-of-go/utils/files"
+	"advent-of-go/utils/slices"
+	"strconv"
+	"strings"
 )
 
 func main() {
@@ -12,16 +15,58 @@ func main() {
 
 func solvePart1(input []string) int {
 	result := 0
-
-
-
+	sequences := parseInput(input)
+	for i := 0; i < len(sequences); i++ {
+		result += solveSequence(sequences[i])
+	}
 	return result
 }
 
 func solvePart2(input []string) int {
 	result := 0
-
-
-
+	sequences := parseInput(input)
+	for i := 0; i < len(sequences); i++ {
+		s := slices.Reverse(sequences[i])
+		result += solveSequence(s)
+	}
 	return result
+}
+
+func parseInput(input []string) [][]int {
+	sequences := make([][]int, len(input))
+	for i := 0; i < len(input); i++ {
+		values := strings.Fields(input[i])
+		sequences[i] = make([]int, len(values))
+		for j := 0; j < len(values); j++ {
+			value, _ := strconv.Atoi(values[j])
+			sequences[i][j] = value
+		}
+	}
+	return sequences
+}
+
+func solveSequence(sequence []int) int {
+	diffs := [][]int{sequence}
+	for !allZeroes(diffs[len(diffs)-1]) {
+		diff := make([]int, len(diffs[len(diffs)-1])-1)
+		for i := 0; i < len(diffs[len(diffs)-1])-1; i++ {
+			diff[i] = diffs[len(diffs)-1][i+1] - diffs[len(diffs)-1][i]
+		}
+		diffs = append(diffs, diff)
+	}
+
+	result := 0
+	for i := 0; i < len(diffs); i++ {
+		result += diffs[i][len(diffs[i])-1]
+	}
+	return result
+}
+
+func allZeroes(sequence []int) bool {
+	for i := 0; i < len(sequence); i++ {
+		if sequence[i] != 0 {
+			return false
+		}
+	}
+	return true
 }
