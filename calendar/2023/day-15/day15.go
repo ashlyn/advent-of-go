9ed7952cf7ac91b3e60da5072cebe088ba7ae12d
Ashlyn Slawnyk <ashlyn.slawnyk@gmail.com>
2023-12-15 15:27:10 +0000

    2023 Day 15

--- calendar/2023/day-15/day15.go
+++ calendar/2023/day-15/day15.go
@@ -2,6 +2,10 @@ package main
 
 import (
 	"advent-of-go/utils/files"
+	"advent-of-go/utils/slices"
+	"fmt"
+	"strconv"
+	"strings"
 )
 
 func main() {
@@ -13,15 +17,74 @@ func main() {
 func solvePart1(input []string) int {
 	result := 0
 
-
+	steps := parseInput(input)
+	for _, s := range steps {
+		result += hash(s)
+	}
 
 	return result
 }
 
 func solvePart2(input []string) int {
-	result := 0
+	steps := parseInput(input)
+	boxes, lenses := buildBoxes(steps)
+	return calculateFocusingPower(boxes, lenses)
+}
 
+func buildKey(label string, box int) string {
+	return fmt.Sprintf("%d-%s", box, label)
+}
 
+func parseInput(input []string) []string {
+	return strings.Split(input[0], ",")
+}
 
-	return result
+func hash(input string) int {
+	currentValue := 0
+	for _, char := range input {
+		currentValue += int(char)
+		currentValue *= 17
+		currentValue %= 256
+	}
+	return currentValue
+}
+
+func buildBoxes(steps []string) ([][]string, map[string]int) {
+	boxes := make([][]string, 256)
+	lenseMap := map[string]int{}
+	for i := 0; i < len(boxes); i++ {
+		boxes[i] = []string{}
+	}
+	for _, s := range steps {
+		if strings.Contains(s, "-") {
+			parts := strings.Split(s, "-")
+			label := parts[0]
+			boxNumber := hash(label)
+			lensIndex := slices.IndexOf(label, boxes[boxNumber])
+			if lensIndex != -1 {
+				boxes[boxNumber] = append(boxes[boxNumber][:lensIndex], boxes[boxNumber][lensIndex+1:]...)
+			}
+		} else if strings.Contains(s, "=") {
+			parts := strings.Split(s, "=")
+			label := parts[0]
+			boxNumber := hash(label)
+			focalLength, _ := strconv.Atoi(parts[1])
+			lenseMap[buildKey(label, boxNumber)] = focalLength
+			if !slices.Contains(boxes[boxNumber], label) {
+				boxes[boxNumber] = append(boxes[boxNumber], label)
+			}
+		}
+	}
+	return boxes, lenseMap
+}
+
+func calculateFocusingPower(boxes [][]string, lensMap map[string]int) int {
+	focusingPower := 0
+	for boxNumber, boxLenses := range boxes {
+		for lensSlot, lensLabel := range boxLenses {
+			focalLength := lensMap[buildKey(lensLabel, boxNumber)]
+			focusingPower += (1 + boxNumber) * (lensSlot + 1) * focalLength
+		}
+	}
+	return focusingPower
 }
