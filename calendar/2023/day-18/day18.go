c35905613df77c4b7614f3a9ea6c7bf599c726c5
Ashlyn Slawnyk <ashlyn.slawnyk@gmail.com>
2023-12-18 19:25:23 +0000

    2023 Day 18 - added geometry library

--- calendar/2023/day-18/day18.go
+++ calendar/2023/day-18/day18.go
@@ -2,6 +2,13 @@ package main
 
 import (
 	"advent-of-go/utils/files"
+	"advent-of-go/utils/grid"
+	"advent-of-go/utils/sets"
+	"math"
+	"strconv"
+	"strings"
+
+	"github.com/twpayne/go-geom"
 )
 
 func main() {
@@ -11,17 +18,123 @@ func main() {
 }
 
 func solvePart1(input []string) int {
-	result := 0
-
-
-
-	return result
+	instructions := parseInstructions(input, parseInstruction)
+	perimeter, minCoords, maxCoords := digPerimeter(instructions)
+	area := getArea(perimeter, minCoords, maxCoords)
+	return area.Size()
 }
 
 func solvePart2(input []string) int {
-	result := 0
+	return useGeometry(parseInstructions(input, parseHexInstruction))
+}
 
+func useGeometry(instructions []instruction) int {
+	x, y := 0, 0
+	vertices := [][]geom.Coord{{{float64(x), float64(y)}}}
+	for _, inst := range instructions {
+		endX, endY := x + (inst.direction.X * inst.spaces), y + (inst.direction.Y * inst.spaces)
+		vertices[0] = append(vertices[0], geom.Coord{float64(endX), float64(endY)})
+		x, y = endX, endY
+	}
+	poly := geom.NewPolygon(geom.XY).MustSetCoords(vertices)
+	totalArea := poly.Area() + (poly.LinearRing(0).Length() / 2) + 1
+	return int(totalArea)
+}
 
+func digPerimeter(instructions []instruction) (sets.Set, grid.Coords, grid.Coords) {
+	maxX, maxY := 0, 0
+	minX, minY := math.MaxInt, math.MaxInt
+	perimeter := sets.New()
+	current := grid.Origin
+	for _, inst := range instructions {
+		for i := 0; i < inst.spaces; i++ {
+			current.X, current.Y = current.X + inst.direction.X, current.Y + inst.direction.Y
+			if current.X > maxX {
+				maxX = current.X
+			}
+			if current.Y > maxY {
+				maxY = current.Y
+			}
+			if current.X < minX {
+				minX = current.X
+			}
+			if current.Y < minY {
+				minY = current.Y
+			}
+			perimeter.Add(current.ToString())
+		}
+	}
+	return perimeter, grid.Coords{ X: minX, Y: minY }, grid.Coords{ X: maxX, Y: maxY }
+}
 
-	return result
+func getArea(perimeter sets.Set, minCoords grid.Coords, maxCoords grid.Coords) sets.Set {
+	area := perimeter.Copy()
+	queue := []grid.Coords{ { X: 1, Y: 1 } }
+	directions := []grid.Coords{ { X: 1, Y: 0 }, { X: -1, Y: 0 }, { X: 0, Y: 1 }, { X: 0, Y: -1 } }
+	for len(queue) > 0 {
+		current := queue[0]
+		queue = queue[1:]
+		for _, direction := range directions {
+			next := grid.Coords{ X: current.X + direction.X, Y: current.Y + direction.Y }
+			if !area.Has(next.ToString()) && !perimeter.Has(next.ToString()) &&
+				next.X >= minCoords.X && next.X <= maxCoords.X &&
+				next.Y >= minCoords.Y && next.Y <= maxCoords.Y {
+				area.Add(next.ToString())
+				queue = append(queue, next)
+			}
+		}
+	}
+	return area
+}
+
+type instruction struct {
+	direction grid.Coords
+	spaces int
+}
+func parseInstructions(input []string, parse func(input string) instruction) []instruction {
+	instructions := make([]instruction, len(input))
+	for i := 0; i < len(input); i++ {
+		instructions[i] = parse(input[i])
+	}
+	return instructions
+}
+func parseInstruction(input string) instruction {
+	parts := strings.Fields(input)
+	direction := grid.Origin
+	switch parts[0] {
+	case "R":
+		direction = grid.Coords{ X: 1, Y: 0 }
+	case "L":
+		direction = grid.Coords{ X: -1, Y: 0 }
+	case "U":
+		direction = grid.Coords{ X: 0, Y: -1 }
+	case "D":
+		direction = grid.Coords{ X: 0, Y: 1 }
+	}
+	spaces, _ := strconv.Atoi(parts[1])
+	return instruction{
+		direction: direction,
+		spaces: spaces,
+	}
+}
+
+func parseHexInstruction(input string) instruction {
+	parts := strings.Fields(input)
+	hex := parts[2][2:len(parts[2]) - 1]
+	direction := grid.Origin
+	switch hex[5] {
+	case '0':
+		direction = grid.Coords{ X: 1, Y: 0 }
+	case '2':
+		direction = grid.Coords{ X: -1, Y: 0 }
+	case '3':
+		direction = grid.Coords{ X: 0, Y: -1 }
+	case '1':
+		direction = grid.Coords{ X: 0, Y: 1 }
+	}
+	spaces, _ := strconv.ParseInt(hex[:5], 16, 64)
+	return instruction{
+		direction: direction,
+		spaces: int(spaces),
+	}
 }
--- go.mod
+++ go.mod
@@ -1,3 +1,7 @@
 module advent-of-go
 
 go 1.18
+
+require (
+	github.com/twpayne/go-geom v1.5.3 // indirect
+)
