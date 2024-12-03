3730748eeab25bc93a32554c4b7f586e8cce1398
Ashlyn Slawnyk <ashlyn.slawnyk@gmail.com>
2024-12-03 15:56:18 +0000

    2024 days 2 and 3

--- calendar/2024/day-02/day02.go
+++ calendar/2024/day-02/day02.go
@@ -2,6 +2,9 @@ package main
 
 import (
 	"advent-of-go/utils/files"
+	"advent-of-go/utils/maths"
+	"advent-of-go/utils/slices"
+	"strings"
 )
 
 func main() {
@@ -13,7 +16,11 @@ func main() {
 func solvePart1(input []string) int {
 	result := 0
 
-
+	for _, line := range input {
+		if isSafe(slices.ParseIntsFromStrings(strings.Fields(line))) {
+			result++
+		}
+	}
 
 	return result
 }
@@ -21,7 +28,39 @@ func solvePart1(input []string) int {
 func solvePart2(input []string) int {
 	result := 0
 
-
+	for _, line := range input {
+		numbers := slices.ParseIntsFromStrings(strings.Fields(line))
+		if isSafe(numbers) {
+			result++
+		} else {
+			for i := 0; i < len(numbers); i++ {
+				amended := make([]int, i)
+				copy(amended, numbers[:i])
+				amended = append(amended, numbers[i+1:]...)
+				if isSafe(amended) {
+					result++
+					break
+				}
+			}
+		}
+	}
 
 	return result
+}
+
+func isSafe(numbers []int) bool {
+	directions := map[int]int{}
+	for i := 0; i < len(numbers) - 1; i++ {
+		diff := numbers[i] - numbers[i + 1]
+		if (diff > 0) {
+			directions[1]++
+		} else if (diff < 0) {
+			directions[-1]++
+		}
+		diffAbs := maths.Abs(diff)
+		if diffAbs > 3 || diffAbs < 1 || len(directions) > 1 {
+			return false
+		}
+	}
+	return true
 }
--- calendar/2024/day-03/day03.go
+++ calendar/2024/day-03/day03.go
@@ -2,6 +2,8 @@ package main
 
 import (
 	"advent-of-go/utils/files"
+	"regexp"
+	"strconv"
 )
 
 func main() {
@@ -11,17 +13,32 @@ func main() {
 }
 
 func solvePart1(input []string) int {
-	result := 0
-
-
-
-	return result
+	return runInstructions(input, false)
 }
 
 func solvePart2(input []string) int {
+	return runInstructions(input, true)
+}
+
+func runInstructions(input []string, useDoDont bool) int {
 	result := 0
 
-
+	instructionsPattern := *regexp.MustCompile(`mul\((\d+),(\d+)\)|do\(\)|don't\(\)`)
+	enabled := true
+	for _, line := range input {
+		matches := instructionsPattern.FindAllStringSubmatch(line, -1)
+		for _, match := range matches {
+			if match[0] == "do()" {
+				enabled = true
+			} else if useDoDont && match[0] == "don't()" {
+				enabled = false
+			} else if enabled {
+				a, _ := strconv.Atoi(match[1])
+				b, _ := strconv.Atoi(match[2])
+				result += a * b
+			}
+		}
+	}
 
 	return result
 }
