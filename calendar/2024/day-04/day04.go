05befc94d0ce4a4bb1eddb1586f7dac6d87a7f67
Ashlyn Slawnyk <ashlyn.slawnyk@gmail.com>
2024-12-04 20:50:27 +0000

    2024 day 4 solutions

--- calendar/2024/day-04/day04.go
+++ calendar/2024/day-04/day04.go
@@ -2,6 +2,10 @@ package main
 
 import (
 	"advent-of-go/utils/files"
+	"advent-of-go/utils/grid"
+	"advent-of-go/utils/maths"
+	"advent-of-go/utils/str"
+	"strings"
 )
 
 func main() {
@@ -13,7 +17,13 @@ func main() {
 func solvePart1(input []string) int {
 	result := 0
 
+	rotated := rotateGrid(input)
 
+	result += findHorizontalOccurances(input, "XMAS")
+	result += findHorizontalOccurances(rotated, "XMAS")
+
+	result += findHorizontalOccurances(buildDiagonals(input), "XMAS")
+	result += findHorizontalOccurances(buildDiagonals(rotated), "XMAS")
 
 	return result
 }
@@ -21,7 +31,62 @@ func solvePart1(input []string) int {
 func solvePart2(input []string) int {
 	result := 0
 
-
+	for y := 1; y < len(input) - 1; y++ {
+		for x := 1; x < len(input[y]) - 1; x++ {
+			if (input[y][x] == 'A') {
+				topLeft, topRight, bottomLeft, bottomRight := input[y - 1][x - 1], input[y - 1][x + 1], input[y + 1][x - 1], input[y + 1][x + 1]
+				if ((topLeft == 'M' && bottomRight == 'S') || (topLeft == 'S' && bottomRight == 'M')) &&
+					((topRight == 'M' && bottomLeft == 'S') || (topRight == 'S' && bottomLeft == 'M')) {
+					result++
+				}
+			}
+		}
+	}
 
 	return result
+}
+
+func findHorizontalOccurances(input []string, searchString string) int {
+	result := 0
+
+	for _, line := range input {
+		result += strings.Count(line, searchString)
+		result += strings.Count(str.Reverse(line), searchString)
+	}
+
+	return result
+}
+
+func rotateGrid(input []string) []string {
+	rotated := []string{}
+	split := [][]string{}
+
+	for _, line := range input {
+		split = append(split, strings.Split(line, ""))
+	}
+
+	rotatedChars := grid.Rotate90(split)
+	for _, line := range rotatedChars {
+		rotated = append(rotated, strings.Join(line, ""))
+	}
+	
+	return rotated
+}
+
+func buildDiagonals(input []string) []string {
+	diagonals := []string{}
+
+	rows, columns := len(input), len(input[0])
+	for line := 1; line <= rows + columns - 1; line++ {
+		start := maths.Max(0, line - rows)
+		items := maths.Min(line, maths.Min(columns - start, rows))
+
+		d := ""
+		for i := 0; i < items; i++ {
+			d += input[maths.Min(rows, line) - i - 1][start + i:start + i + 1]
+		}
+		diagonals = append(diagonals, d)
+	}
+
+	return diagonals
 }
