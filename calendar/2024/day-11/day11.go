30384dc7f7bd8638850ba1932833b06a2495de35
Ashlyn Slawnyk <ashlyn.slawnyk@gmail.com>
2024-12-11 17:18:26 +0000

    2024 day 11 solutions

--- calendar/2024/day-11/day11.go
+++ calendar/2024/day-11/day11.go
@@ -2,6 +2,9 @@ package main
 
 import (
 	"advent-of-go/utils/files"
+	"fmt"
+	"strconv"
+	"strings"
 )
 
 func main() {
@@ -11,17 +14,51 @@ func main() {
 }
 
 func solvePart1(input []string) int {
-	result := 0
-
-
-
-	return result
+	stonesCounts := parseInput(input[0])
+	return applyRulesToStonesNTimes(stonesCounts, 25)
 }
 
 func solvePart2(input []string) int {
+	stonesCounts := parseInput(input[0])
+	return applyRulesToStonesNTimes(stonesCounts, 75)
+}
+
+func parseInput(input string) map[string]int {
+	stonesArr := strings.Fields(input)
+	stones := map[string]int{}
+	for i := 0; i < len(stonesArr); i++ {
+		stones[stonesArr[i]]++
+	}
+	return stones
+}
+
+func applyRules(stones map[string]int) map[string]int {
+	newStones := map[string]int{}
+	for stone, count := range stones {
+		if stone == "0" {
+			newStones["1"] += count
+		} else if len(stone) % 2 == 0 {
+			firstHalf := stone[:len(stone) / 2]
+			secondValue, _ := strconv.Atoi(stone[len(stone) / 2:])
+			secondHalf := fmt.Sprintf("%d", secondValue)
+			newStones[firstHalf] += count
+			newStones[secondHalf] += count
+		} else {
+			newValue, _ := strconv.Atoi(stone)
+			newStones[fmt.Sprintf("%d", newValue * 2024)] += count
+		}
+	}
+	return newStones
+}
+
+func applyRulesToStonesNTimes(stonesCounts map[string]int, n int) int {
+	for i := 0; i < n; i++ {
+		stonesCounts = applyRules(stonesCounts)
+	}
+
 	result := 0
-
-
-
+	for _, count := range stonesCounts {
+		result += count
+	}
 	return result
 }
