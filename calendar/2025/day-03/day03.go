f28500b32ade500dbb8dfd462d481fe334520e83
Ashlyn Slawnyk <ashlyn.slawnyk@gmail.com>
2025-12-03 15:15:55 +0000

    2025 Day 3 solutions

--- calendar/2025/day-03/day03.go
+++ calendar/2025/day-03/day03.go
@@ -2,6 +2,8 @@ package main
 
 import (
 	"advent-of-go/utils/files"
+	"advent-of-go/utils/maths"
+	"advent-of-go/utils/slices"
 )
 
 func main() {
@@ -13,7 +15,11 @@ func main() {
 func solvePart1(input []string) int {
 	result := 0
 
-
+	banks := parseInput(input)
+	
+	for i := range banks {
+		result += maxJoltage(banks[i], 2)
+	}
 
 	return result
 }
@@ -21,7 +27,33 @@ func solvePart1(input []string) int {
 func solvePart2(input []string) int {
 	result := 0
 
-
+	banks := parseInput(input)
+	
+	for i := range banks {
+		result += maxJoltage(banks[i], 12)
+	}
 
 	return result
+}
+
+func parseInput(input []string) [][]int {
+	parsed := make([][]int, len(input))
+	for i, line := range input {
+		nums := make([]int, len(line))
+		for j, char := range line {
+			nums[j] = int(char - '0')
+		}
+		parsed[i] = nums
+	}
+	return parsed
+}
+
+func maxJoltage(bank []int, n int) int {
+	if n == 1 {
+		return slices.Max(bank)
+	}
+	front := bank[:len(bank)-(n-1)]
+	max := slices.Max(front)
+	maxIndex := slices.IndexOfInt(max, front)
+	return (max * maths.Pow(10, n-1)) + maxJoltage(bank[maxIndex+1:], n-1)
 }
