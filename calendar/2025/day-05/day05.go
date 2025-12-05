c267e04fcc43d14d2f342836e34284c04a498931
Ashlyn Slawnyk <ashlyn.slawnyk@gmail.com>
2025-12-05 16:32:54 +0000

    2025 Day 5 part 2

--- calendar/2025/day-05/day05.go
+++ calendar/2025/day-05/day05.go
@@ -4,6 +4,7 @@ import (
 	"advent-of-go/utils/files"
 	"advent-of-go/utils/slices"
 	"fmt"
+	"sort"
 	"strings"
 	"time"
 )
@@ -32,11 +33,25 @@ func solvePart1(input []string) int {
 }
 
 func solvePart2(input []string) int {
-	result := 0
+	ranges, _ := parseInput(input)
 
+	// sort ranges by min value so that we can simplify without backtracking
+	sort.Slice(ranges, func(i, j int) bool {
+		return ranges[i][0] < ranges[j][0]
+	})
 
+	// iterate over ranges, simplifying down to non-overlapping ranges
+	for i := 0; i < len(ranges)-1; {
+		simplified := simplifyTwoRanges(ranges[i], ranges[i+1])
+		if len(simplified) == 1 {
+			ranges[i] = simplified[0]
+			ranges = append(ranges[:i+1], ranges[i+2:]...)
+		} else {
+			i++
+		}
+	}
 
-	return result
+	return countIngredientsInRanges(ranges)
 }
 
 func parseInput(input []string) ([][2]int, []int) {
@@ -67,4 +82,34 @@ func isFresh(ingredient int, ranges [][2]int) bool {
 		}
 	}
 	return false
+}
+
+func simplifyTwoRanges(r1, r2 [2]int) [][2]int {
+	// no overlap
+	if r2[1] < r1[0] || r1[1] < r2[0] {
+		return [][2]int{r1, r2}
+	}
+	// complete overlap, r2 in r1
+	if r1[0] <= r2[0] && r1[1] >= r2[1] {
+		return [][2]int{r1}
+	}
+	// complete overlap, r1 in r2
+	if r2[0] <= r1[0] && r2[1] >= r1[1] {
+		return [][2]int{r2}
+	}
+	// partial overlap
+	newRange := [2]int{slices.Min([]int{r1[0], r2[0]}), slices.Max([]int{r1[1], r2[1]})}
+	return [][2]int{newRange}
+}
+
+func countIngredientsInRange(r [2]int) int {
+	return r[1] - r[0] + 1
+}
+
+func countIngredientsInRanges(ranges [][2]int) int {
+	count := 0
+	for i := 0; i < len(ranges); i++ {
+		count += countIngredientsInRange(ranges[i])
+	}
+	return count
 }
