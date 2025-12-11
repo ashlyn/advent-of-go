6eacec5f7e7a86d48601d5ad85e5a79337c949dd
Ashlyn Slawnyk <ashlyn.slawnyk@gmail.com>
2025-12-11 15:01:57 +0000

    2025 Day 11 pt 1

--- calendar/2025/day-11/day11.go
+++ calendar/2025/day-11/day11.go
@@ -2,6 +2,7 @@ package main
 
 import (
 	"advent-of-go/utils/files"
+	"strings"
 )
 
 func main() {
@@ -11,11 +12,9 @@ func main() {
 }
 
 func solvePart1(input []string) int {
-	result := 0
-
-
-
-	return result
+	adjacencyList := parseInput(input)
+	validPaths := bfs(adjacencyList, "you", "out")
+	return len(validPaths)
 }
 
 func solvePart2(input []string) int {
@@ -24,4 +23,48 @@ func solvePart2(input []string) int {
 
 
 	return result
+}
+
+func parseInput(input []string) map[string][]string {
+	adjacencyList := map[string][]string{}
+	for _, line := range input {
+		parts := strings.Fields(line)
+		device := parts[0][0:len(parts[0])-1]
+		outputs := parts[1:]
+		adjacencyList[device] = outputs
+	}
+	return adjacencyList
+}
+
+func bfs(adjacencyList map[string][]string, start, end string) [][]string {
+	validPaths := [][]string{}
+	visited := map[string]bool{}
+
+	queue := [][]string{{start}}
+
+	for len(queue) > 0 {
+		currentPath := queue[0]
+		queue = queue[1:]
+		device := currentPath[len(currentPath)-1]
+
+		if device == end {
+			validPaths = append(validPaths, currentPath)
+			continue
+		}
+
+		pathKey := strings.Join(currentPath, "-")
+		if !visited[pathKey] {
+			visited[pathKey] = true
+			for _, output := range adjacencyList[device] {
+				if !strings.Contains(pathKey, output) {
+					newPath := make([]string, len(currentPath))
+					copy(newPath, currentPath)
+					newPath = append(newPath, output)
+					queue = append(queue, newPath)
+				}
+			}
+		}
+	}
+
+	return validPaths
 }
